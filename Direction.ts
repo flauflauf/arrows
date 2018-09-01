0112fa619c8ca2d28e4596561162d09cffadd825
Florian von Stosch <florian.vonstosch@wps.de>
2018-09-01 13:13:08 +0200

    Aufteilung in mehrere Dateien

---
+++ Direction.ts
@@ -0,0 +1,7 @@
+enum Direction {
+    None    = 0,
+    Up      = 1 << 0,
+    Down    = 1 << 1,
+    Left    = 1 << 2,
+    Right   = 1 << 3
+}
--- arrows.ts
+++ arrows.ts
@@ -1,14 +1,6 @@
 let canv
 let ctx
 
-enum Direction {
-    None    = 0,
-    Up      = 1 << 0,
-    Down    = 1 << 1,
-    Left    = 1 << 2,
-    Right   = 1 << 3
-}
-
 class Vector {
     static ZERO = new Vector(0, 0)
 
