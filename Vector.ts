31dfd414ebd743e50a8a3c39632156ec113d3a44
Florian von Stosch <florian.vonstosch@wps.de>
2018-09-12 18:38:59 +0200

    Diagonal bewegen ist nicht mehr schneller

--- Vector.ts
+++ Vector.ts
@@ -12,9 +12,18 @@ class Vector {
             v.x -= 1
         if (orientation & Direction.Right)
             v.x += 1
-        return v
+        return v.normalize()
     }
     multiply(scalarFactor: number) {
         return new Vector(this.x * scalarFactor, this.y * scalarFactor)
+    }
+    normalize() : Vector {
+        let norm = this.norm()
+        return norm === 0 ? this : this.multiply(1 / norm)
+    }
+    norm() : number {
+        let norm = Math.sqrt(this.x * this.x + this.y * this.y)
+        console.log("norm: " + norm);
+        return norm
     }
 }
